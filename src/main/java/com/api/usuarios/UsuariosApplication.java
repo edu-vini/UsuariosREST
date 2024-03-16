485604f52aacb97cde5cf025630af213241a4e40
edu-vini <vinyeduardo95@gmail.com>
2024-03-16 09:48:11 -0300

    2024-03-16

--- src/main/java/com/api/usuarios/UsuariosApplication.java
+++ src/main/java/com/api/usuarios/UsuariosApplication.java
@@ -1,7 +1,11 @@
 package com.api.usuarios;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 
@@ -11,4 +15,21 @@ public class UsuariosApplication {
 		SpringApplication.run(UsuariosApplication.class, args);
 	}
 
+	@Bean
+	public GroupedOpenApi publicApi(){
+		return GroupedOpenApi.builder()
+				.group("controller")
+				.packagesToScan("com.api.usuarios.controller")
+				.build();
+	}
+
+	@Bean
+	public OpenAPI usuarioApi(){
+		return new OpenAPI()
+				.info(new Info().title("Usuários API")
+						.description("Api para retornar dados de usuário")
+						.version("0.0.1")
+				);
+
+	}
 }
